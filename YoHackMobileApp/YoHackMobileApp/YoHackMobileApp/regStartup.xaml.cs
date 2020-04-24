95fa2797eda25eedbe00570e77f341b738e597fc
Daniel Khromov <danielkhromov@gmail.com>
2020-04-25 02:06:19 +0300

    initial commit

---
+++ YoHackMobileApp/YoHackMobileApp/YoHackMobileApp/regStartup.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace YoHackMobileApp
+{
+    public partial class regStartup : ContentPage
+    {
+        public regStartup()
+        {
+            InitializeComponent();
+        }
+        async void Save(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new startupHome());
+        }
+    }
+}
---
+++ YoHackMobileApp/YoHackMobileApp/YoHackMobileApp/startupHome.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace YoHackMobileApp
+{
+    public partial class startupHome : ContentPage
+    {
+        public startupHome()
+        {
+            InitializeComponent();
+        }
+        //remove back btn
+        protected override bool OnBackButtonPressed()
+        {
+            return true;
+        }
+    }
+}
